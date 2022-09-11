a9ffc8b6ce29a2fda4970360ab9affca05dd665a
Gustavo Miyabe <100000507+gusthavo123@users.noreply.github.com>
2022-09-11 08:05:32 -0300

    Update atv5.java

--- Lista7/src/Atividades/atv5.java
+++ Lista7/src/Atividades/atv5.java
@@ -4,23 +4,25 @@ import java.util.Scanner;
 
 public class atv5  {
 	public static void main(String[] args) {
-		Scanner ler = new Scanner (System.in);
-		int no;
-		int number = 1;
-		System.out.println("Coloque um numero: ");
-		 no = ler.nextInt ();
-		int spaces = no;
-		for (int i = 0; i < no; i++ ) {
-			for (int s = 1; s <= spaces; s ++) {
-				System.out.println(" ");
-			}
-			number = 1;
-			for (int j = 0; j <= i; j ++) {
-				System.out.println(number + " ");
-				number = number * (i - j) / (j + i);
-			}
-		spaces --;
-		}
+		 Scanner ler = new Scanner(System.in);
+
+	        System.out.println("Insira um numero: ");
+	        int num = ler.nextInt();
+
+	        int vetor[] = new int[num];
+
+	        vetor[0] = 1;
+	        for (int i = 0; i < vetor.length; i++) {
+	            System.out.println();
+	            int aux1 = 0;
+	            int aux2;
+	            for (int j = 0; j <= i; j++) {
+	                aux2 = aux1 + vetor[j];
+	                aux1 = vetor[j];
+	                vetor[j] = aux2;
+	                System.out.print(vetor[j] + " ");
+	            }
+	        }
 	}
 }
 
