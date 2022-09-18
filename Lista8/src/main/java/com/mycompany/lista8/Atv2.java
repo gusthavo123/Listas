9103c2e98a32a45702844dcc672a2f49d695de29
Gustavo Miyabe <100000507+gusthavo123@users.noreply.github.com>
2022-09-18 08:38:24 -0300

    Update Atv2.java

--- Lista8/src/main/java/com/mycompany/lista8/Atv2.java
+++ Lista8/src/main/java/com/mycompany/lista8/Atv2.java
@@ -1,21 +1,25 @@
 
 package com.mycompany.lista8;
 import java.util.Scanner;
-/**
- *
- * @author labinfo
- */
+
 public class Atv2 {
     public static void main(String[] args) {
         Scanner ler = new Scanner (System.in);
         int A [][] = new int [5] [5];
-        for (int i = 0; i < 5; i ++){
-          for (int j = 0; j < 5; j ++){
-              System.out.println("Informe a linha: " + i + " º" + " e a coluna: " + j + " º");
-              A [i][j] = ler.nextInt ();
-              A[i][j] = i++;
-              System.out.println(A[i][j]);
+        for (int i = 0; i < A.lenght; i ++){
+          for (int j = 0; j < A.lenght; j ++){
+             if (j == 1){
+                 A[i][j] = 1;
+             } else {
+                 A[i][j] = 0;
+             }
           }  
+        }
+        for (int i < 0; i < A.lenght; i++){
+            for (int j < 0; i <A.lenght; i ++){
+                System.out.print (A[i][j]) + "");
+            }
+            System.out.println ():
         }
     }
 }
