0e66017059a9bfae0a838cc329b6b18232c6e32d
Gustavo Miyabe <100000507+gusthavo123@users.noreply.github.com>
2022-09-18 08:33:58 -0300

    Update Lista8.java

--- Lista8/src/main/java/com/mycompany/lista8/Lista8.java
+++ Lista8/src/main/java/com/mycompany/lista8/Lista8.java
@@ -10,14 +10,16 @@ public class Lista8 {
     public static void main(String[] args) {
         Scanner ler = new Scanner (System.in);
         int A [][] = new int [4][4];
+        int maior10 = 0;
         for (int i = 0; i < 4; i ++){
             for (int j = 0; j < 4; j ++){
              System.out.println("informe a linha: " + i + " e coluna: " + j);
              A[i][j] = ler.nextInt ();
             if(A[i][j] > 10){
-            System.out.println ("O números maiores que 10 são:" + A[i][j]);
+            maior10++;
             }
          }
         }
+        System.out.println ("Os números maiores que 10 são: " + maior10);
       }
     }
